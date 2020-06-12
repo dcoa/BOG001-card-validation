2494feb49c2837c6fd2c9b91db2752a6d1bd42ea
dcoa <dcoa@live.com>
2020-06-12 10:28:37 -0500

    imagenes, html, css y prueba js no corre

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import validator from './validator.js';
 
+function enviar () {
+  let cardCreditCard =  "1234567890987654";
+  validator.isValid(cardCreditCard);
+}
+
+
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,39 @@
 const validator = {
-  // ...
+    isValid: isValid(),
 };
+
+function isValid(){
+  let cardNumber = Array.from();
+  let reverseNumber = cardNumber.reverse();
+  let sumNumber = [];
+
+  reverseNumber.forEach((a, i) => {
+  if (a, i % 2 !== 0) {
+    a = parseInt(a * 2);
+      if (a > 9) {
+      a = sumaDigit(a);
+    }
+  }
+   sumNumber.push(parseInt(a));
+});
+  validate(sumNumber);
+}
+
+ function sumaDigit(a) {
+        let numstring = a.toString();
+        let c = parseInt(numstring.charAt(0));
+        let b = parseInt(numstring.charAt(1));
+        a = c + b;
+      return a;
+ }
+
+function validate(sumNumber) {
+  let total = sumNumber.reduce((a, b) => a + b, 0);
+  if (total % 10 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 export default validator;
